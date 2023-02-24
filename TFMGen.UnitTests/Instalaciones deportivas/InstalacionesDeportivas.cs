1a54d205fc08779c5f337bd0f9a3a6b73be1f5fc
Oscar Martínez Martínez <omm35@mscloud.ua.es>
2023-02-24 19:14:37 +0100

    Contexto y primera prueba

---
+++ TFMGen.UnitTests/Instalaciones deportivas/InstalacionesDeportivas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFMGen.ApplicationCore.CEN.TFM;
+using TFMGen.ApplicationCore.EN.TFM;
+using TFMGen.Infraestructure.Repository.TFM;
+
+namespace TFMGen.UnitTests.Instalaciones_deportivas
+{
+    [TestClass]
+    public class InstalacionesDeportivas
+    {
+        Gestor db;
+
+        public InstalacionesDeportivas()
+        {
+            this.db = new Gestor();    
+        }
+
+        [TestMethod]
+        public void CrearInstalacion()
+        {
+            EntidadEN entidad = this.db.entidadcen.Obtener(196609);
+            int idInstalacion = this.db.instalacioncen.Crear("Instalación de padel", entidad.Identidad, "666666666", "Calle padel", null, "03801", "Alcoy", "Alicante", null);
+            InstalacionEN instalacionEn = this.db.instalacioncen.Obtener(idInstalacion);
+
+            Assert.AreEqual(true, instalacionEn != null);
+        }
+    }
+}
