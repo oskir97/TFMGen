576dbba52466412f1d489f7b4bedb619c8bd616f
DESKTOP-5Q3SGUK\Santi <santicastalla15@gmail.com>
2023-03-09 17:26:32 +0100

    Pruebas Instructores + Pistas

--- TFMGen.UnitTests/Instructores/Instructores.cs
+++ TFMGen.UnitTests/Instructores/Instructores.cs
@@ -41,5 +41,53 @@ namespace TFMGen.UnitTests.Instructores
 
             Assert.AreEqual(true, usuarioEn != null);
         }
+
+        [TestMethod]
+        public void ModificarInstructor()
+        {
+            int rolid = this.db.rolcen.Listar(1, 0).Where(x => x.Nombre == "Entrenador").Select(x => x.Idrol).First();
+            UsuarioEN instructorModificar = this.db.usuariocen.Listar(1, 0).Where(x => x.Rol.Idrol == rolid).First();
+
+            UsuarioEN instructorModificada = instructorModificar;
+            instructorModificada.Nombre = "Instructor Modificado";
+
+            this.db.usuariocen.Editar(instructorModificada.Idusuario, instructorModificada.Nombre, instructorModificada.Email, instructorModificada.Domicilio,
+                instructorModificada.Telefono, DateTime.Now, DateTime.Now, instructorModificada.Apellidos, instructorModificada.Password, instructorModificada.Codigopostal,
+                instructorModificada.Localidad, instructorModificada.Provincia, instructorModificada.Telefonoalternativo);
+
+            Assert.AreEqual(instructorModificar,instructorModificada);
+        }
+
+        //[TestMethod]
+        //public void CambiarNormalaInstructor()
+        //{
+        //    //
+        //}
+
+        [TestMethod]
+        public void EliminarInstructor()
+        {
+            int rolid = this.db.rolcen.Listar(1, 0).Where(x => x.Nombre == "Entrenador").Select(x => x.Idrol).First();
+            int idUsuario = this.db.usuariocen.Crear(
+                "Andy",
+                "ejemplo@test.com",
+                "Calle San Juan",
+                "666777888",
+                DateTime.Now,
+                DateTime.Now,
+                "McAllow",
+                "Password",
+                rolid,
+                "03450",
+                "Banyeres",
+                "Alicante",
+                "123456789");
+            UsuarioEN usuarioEn = this.db.usuariocen.Obtener(idUsuario);
+            this.db.usuariocen.Eliminar(idUsuario);
+
+
+            UsuarioEN usuarioError = this.db.usuariocen.Obtener(idUsuario);
+            Assert.AreEqual(usuarioError, null);
+        }
     }
 }
---
+++ TFMGen.UnitTests/Pistas/Pistas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFMGen.ApplicationCore.CEN.TFM;
+using TFMGen.ApplicationCore.EN.TFM;
+using TFMGen.ApplicationCore.Exceptions;
+using TFMGen.Infraestructure.Repository.TFM;
+
+namespace TFMGen.UnitTests.Instalaciones_deportivas
+{
+    [TestClass]
+    public class Pistas
+    {
+        Context db;
+
+        public Pistas()
+        {
+            this.db = new Context();
+        }
+
+        [TestMethod]
+        public void CrearPistaenInstalacion()
+        {
+            EntidadEN entidad = this.db.entidadcen.Listar(0, 1).First();
+            PistaEstadoEN estadosPista = this.db.pistaestadocen.Listar(0, 1).First();
+
+            int idInstalacion = this.db.instalacioncen.Crear("Instalación de padel", entidad.Identidad, "666666666", "Calle padel", null, "03801", "Alcoy", "Alicante", null, true);
+            InstalacionEN instalacionEn = this.db.instalacioncen.Obtener(idInstalacion);
+            int idPista = this.db.pistacen.Crear("prueba", 1, entidad.Identidad, estadosPista.Idestado, null, "Tibi", false);
+            PistaEN pista = this.db.pistacen.Obtener(idPista);
+
+            instalacionEn.Pistas.Add(pista);
+
+
+
+            Assert.IsTrue(instalacionEn.Pistas.Contains(pista));
+
+        }
+
+       
+    }
+}
