977b48125a78965e20b286284dd8a6da44ceefd6
oskir97 <oscarmarmar97@gmail.com>
2023-06-08 19:12:59 +0200

    correcciones distancias filtros

--- TFMGen.ApplicationCore/CP/TFM/EventoCP_listarfiltros.cs
+++ TFMGen.ApplicationCore/CP/TFM/EventoCP_listarfiltros.cs
@@ -19,114 +19,110 @@ using Geolocation;
 
 namespace TFMGen.ApplicationCore.CP.TFM
 {
-public partial class EventoCP : GenericBasicCP
-{
-public System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.EventoEN> Listarfiltros (string filtro, string localidad, string latitud, string longitud, int deporte, string orden, Nullable<DateTime> fecha, bool notClose = false)
-{
-        /*PROTECTED REGION ID(TFMGen.ApplicationCore.CP.TFM_Evento_listarfiltros) ENABLED START*/
-
-        EventoCEN eventoCEN = null;
-
-        System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.EventoEN>  result = null;
-
-
-        try
+    public partial class EventoCP : GenericBasicCP
+    {
+        public System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.EventoEN> Listarfiltros(string filtro, string localidad, string latitud, string longitud, int deporte, string orden, Nullable<DateTime> fecha, bool notClose = false)
         {
+            /*PROTECTED REGION ID(TFMGen.ApplicationCore.CP.TFM_Evento_listarfiltros) ENABLED START*/
+
+            EventoCEN eventoCEN = null;
+
+            System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.EventoEN> result = null;
+
+
+            try
+            {
                 if (!notClose)
-                    CPSession.SessionInitializeTransaction ();
-                eventoCEN = new  EventoCEN (unitRepo.eventorepository);
-                unitRepo.eventorepository.setSessionCP (CPSession);
+                    CPSession.SessionInitializeTransaction();
+                eventoCEN = new EventoCEN(unitRepo.eventorepository);
+                unitRepo.eventorepository.setSessionCP(CPSession);
 
                 if (!fecha.HasValue)
-                        fecha = DateTime.Today;
+                    fecha = DateTime.Today;
 
-                var eventos = eventoCEN.Listartodos (0, -1).Where (e => e.Deporte.Iddeporte == deporte && (e.Inicio <= fecha.Value && e.Fin >= fecha.Value) && e.Reservas.Count () < e.Plazas && ((e.Instalacion != null && e.Instalacion.Localidad.Contains (localidad)) || (e.Entidad.Localidad.Contains (localidad))) && (!string.IsNullOrEmpty (filtro) ? e.Horarios.Any (h => h.Pista.Nombre.Contains (filtro)) || e.Entidad.Nombre.Contains (filtro) || (e.Instalacion != null && (e.Instalacion.Nombre.Contains (filtro))) || e.Entidad.Cifnif.Contains (filtro) || e.Entidad.Localidad.Contains (filtro) : true)).ToList ();
+                var eventos = eventoCEN.Listartodos(0, -1).Where(e => e.Deporte.Iddeporte == deporte && (e.Inicio <= fecha.Value && e.Fin >= fecha.Value) && e.Reservas.Count() < e.Plazas && ((e.Instalacion != null && e.Instalacion.Localidad.Contains(localidad)) || (e.Entidad.Localidad.Contains(localidad))) && (!string.IsNullOrEmpty(filtro) ? e.Horarios.Any(h => h.Pista.Nombre.Contains(filtro)) || e.Entidad.Nombre.Contains(filtro) || (e.Instalacion != null && (e.Instalacion.Nombre.Contains(filtro))) || e.Entidad.Cifnif.Contains(filtro) || e.Entidad.Localidad.Contains(filtro) : true)).ToList();
 
-                if (!string.IsNullOrEmpty (orden)) {
-                        switch (orden.ToLower ()) {
+                if (!string.IsNullOrEmpty(orden))
+                {
+                    switch (orden.ToLower())
+                    {
                         case "distancia":
                         default:
-                                if (!string.IsNullOrEmpty (latitud) && !string.IsNullOrEmpty (longitud)) {
-                                        double latitudDouble;
-                                        double longitudDouble;
+                            if (!string.IsNullOrEmpty(latitud) && !string.IsNullOrEmpty(longitud))
+                            {
+                                double latitudDouble;
+                                double longitudDouble;
 
-                                        if (double.TryParse (latitud, out latitudDouble) && double.TryParse (longitud, out longitudDouble)) {
-                                                eventos.Sort ((reserva1, reserva2) =>
-                                                        {
-                                                                double latitud1 = reserva1.Instalacion != null ? reserva1.Instalacion.Latitud : reserva1.Instalacion.Latitud;
-                                                                double longitud1 = reserva1.Instalacion != null ? reserva1.Instalacion.Longitud : reserva1.Instalacion.Longitud;
-                                                                double latitud2 = reserva2.Instalacion != null ? reserva2.Instalacion.Latitud : reserva2.Instalacion.Latitud;
-                                                                double longitud2 = reserva2.Instalacion != null ? reserva2.Instalacion.Longitud : reserva2.Instalacion.Longitud;
-
-                                                                double distancia1 = GeoCalculator.GetDistance (latitudDouble, longitudDouble, latitud1, longitud1, 1, DistanceUnit.Kilometers);
-                                                                double distancia2 = GeoCalculator.GetDistance (latitudDouble, longitudDouble, latitud2, longitud2, 1, DistanceUnit.Kilometers);
-
-                                                                return distancia1.CompareTo (distancia2);
-                                                        });
-                                        }
+                                if (double.TryParse(latitud, out latitudDouble) && double.TryParse(longitud, out longitudDouble))
+                                {
+                                    eventos.OrderBy(reserva =>
+                                    {
+                                        double latitud1 = reserva.Instalacion != null ? reserva.Instalacion.Latitud : reserva.Instalacion.Latitud;
+                                        double longitud1 = reserva.Instalacion != null ? reserva.Instalacion.Longitud : reserva.Instalacion.Longitud;
+                                        double distancia = GeoCalculator.GetDistance(latitudDouble, longitudDouble, latitud1, longitud1, 1, DistanceUnit.Kilometers);
+                                        return distancia;
+                                    }).ToList();
                                 }
-                                break;
+                            }
+                            break;
 
                         case "distancia desc":
-                                if (!string.IsNullOrEmpty (latitud) && !string.IsNullOrEmpty (longitud)) {
-                                        double latitudDouble;
-                                        double longitudDouble;
+                            if (!string.IsNullOrEmpty(latitud) && !string.IsNullOrEmpty(longitud))
+                            {
+                                double latitudDouble;
+                                double longitudDouble;
 
-                                        if (double.TryParse (latitud, out latitudDouble) && double.TryParse (longitud, out longitudDouble)) {
-                                                eventos.Sort ((reserva1, reserva2) =>
-                                                        {
-                                                                double latitud1 = reserva1.Instalacion != null ? reserva1.Instalacion.Latitud : reserva1.Instalacion.Latitud;
-                                                                double longitud1 = reserva1.Instalacion != null ? reserva1.Instalacion.Longitud : reserva1.Instalacion.Longitud;
-                                                                double latitud2 = reserva2.Instalacion != null ? reserva2.Instalacion.Latitud : reserva2.Instalacion.Latitud;
-                                                                double longitud2 = reserva2.Instalacion != null ? reserva2.Instalacion.Longitud : reserva2.Instalacion.Longitud;
-
-                                                                double distancia1 = GeoCalculator.GetDistance (latitudDouble, longitudDouble, latitud1, longitud1, 1, DistanceUnit.Kilometers);
-                                                                double distancia2 = GeoCalculator.GetDistance (latitudDouble, longitudDouble, latitud2, longitud2, 1, DistanceUnit.Kilometers);
-
-                                                                return distancia2.CompareTo (distancia1);
-                                                        });
-                                        }
+                                if (double.TryParse(latitud, out latitudDouble) && double.TryParse(longitud, out longitudDouble))
+                                {
+                                    eventos.OrderByDescending(reserva =>
+                                    {
+                                        double latitud1 = reserva.Instalacion != null ? reserva.Instalacion.Latitud : reserva.Instalacion.Latitud;
+                                        double longitud1 = reserva.Instalacion != null ? reserva.Instalacion.Longitud : reserva.Instalacion.Longitud;
+                                        double distancia = GeoCalculator.GetDistance(latitudDouble, longitudDouble, latitud1, longitud1, 1, DistanceUnit.Kilometers);
+                                        return distancia;
+                                    }).ToList();
                                 }
-                                break;
+                            }
+                            break;
 
                         case "precio":
-                                eventos = eventos.OrderBy (r => r.Precio).ToList ();
-                                break;
+                            eventos = eventos.OrderBy(r => r.Precio).ToList();
+                            break;
 
                         case "precio desc":
-                                eventos = eventos.OrderByDescending (r => r.Precio).ToList ();
-                                break;
+                            eventos = eventos.OrderByDescending(r => r.Precio).ToList();
+                            break;
 
                         case "valoracion":
-                                eventos = eventos.OrderBy (r => r.Valoraciones.Sum (v => v.Estrellas)).ToList ();
-                                break;
+                            eventos = eventos.OrderBy(r => r.Valoraciones.Sum(v => v.Estrellas)).ToList();
+                            break;
 
                         case "valoracion desc":
-                                eventos = eventos.OrderByDescending (r => r.Valoraciones.Sum (v => v.Estrellas)).ToList ();
-                                break;
-                        }
+                            eventos = eventos.OrderByDescending(r => r.Valoraciones.Sum(v => v.Estrellas)).ToList();
+                            break;
+                    }
                 }
 
                 result = eventos;
 
                 if (!notClose)
-                    CPSession.Commit ();
-        }
-        catch (Exception ex)
-        {
+                    CPSession.Commit();
+            }
+            catch (Exception ex)
+            {
                 if (!notClose)
-                    CPSession.RollBack ();
+                    CPSession.RollBack();
                 throw ex;
-        }
-        finally
-        {
+            }
+            finally
+            {
                 if (!notClose)
-                    CPSession.SessionClose ();
+                    CPSession.SessionClose();
+            }
+            return result;
+
+
+            /*PROTECTED REGION END*/
         }
-        return result;
-
-
-        /*PROTECTED REGION END*/
-}
-}
+    }
 }
--- TFMGen.ApplicationCore/CP/TFM/ReservaCP_listarfiltros.cs
+++ TFMGen.ApplicationCore/CP/TFM/ReservaCP_listarfiltros.cs
@@ -19,114 +19,110 @@ using Geolocation;
 
 namespace TFMGen.ApplicationCore.CP.TFM
 {
-public partial class ReservaCP : GenericBasicCP
-{
-public System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.ReservaEN> Listarfiltros (string filtro, string localidad, string latitud, string longitud, Nullable<DateTime> fecha, int deporte, string orden, bool notClose = false)
-{
-        /*PROTECTED REGION ID(TFMGen.ApplicationCore.CP.TFM_Reserva_listarfiltros) ENABLED START*/
-
-        ReservaCEN reservaCEN = null;
-
-        System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.ReservaEN> result = null;
-
-
-        try
+    public partial class ReservaCP : GenericBasicCP
+    {
+        public System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.ReservaEN> Listarfiltros(string filtro, string localidad, string latitud, string longitud, Nullable<DateTime> fecha, int deporte, string orden, bool notClose = false)
         {
+            /*PROTECTED REGION ID(TFMGen.ApplicationCore.CP.TFM_Reserva_listarfiltros) ENABLED START*/
+
+            ReservaCEN reservaCEN = null;
+
+            System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.ReservaEN> result = null;
+
+
+            try
+            {
                 if (!notClose)
-                    CPSession.SessionInitializeTransaction ();
-                reservaCEN = new ReservaCEN (unitRepo.reservarepository);
-                unitRepo.reservarepository.setSessionCP (CPSession);
+                    CPSession.SessionInitializeTransaction();
+                reservaCEN = new ReservaCEN(unitRepo.reservarepository);
+                unitRepo.reservarepository.setSessionCP(CPSession);
 
                 if (!fecha.HasValue)
-                        fecha = DateTime.Today;
+                    fecha = DateTime.Today;
 
-                var reservas = reservaCEN.Listartodos (0, -1).Where (r => r.Deporte.Iddeporte == deporte && r.Partido.Tipo == Enumerated.TFM.TipoReservaEnum.partido && r.Fecha == fecha.Value && r.Inscripciones.Count () < r.Maxparticipantes && (!string.IsNullOrEmpty (filtro) ? r.Pista.Nombre.Contains (filtro) || r.Pista.Instalacion.Nombre.Contains (filtro) || r.Pista.Instalacion.Entidad.Nombre.Contains (filtro) || r.Pista.Instalacion.Entidad.Cifnif.Contains (filtro) : true) && ((r.Pista.Instalacion != null && r.Pista.Instalacion.Localidad.ToLower ().Contains (localidad.ToLower ())) || (r.Pista.Entidad.Localidad.Contains (localidad)))).ToList ();
+                var reservas = reservaCEN.Listartodos(0, -1).Where(r => r.Deporte.Iddeporte == deporte && r.Partido.Tipo == Enumerated.TFM.TipoReservaEnum.partido && r.Fecha == fecha.Value && r.Inscripciones.Count() < r.Maxparticipantes && (!string.IsNullOrEmpty(filtro) ? r.Pista.Nombre.Contains(filtro) || r.Pista.Instalacion.Nombre.Contains(filtro) || r.Pista.Instalacion.Entidad.Nombre.Contains(filtro) || r.Pista.Instalacion.Entidad.Cifnif.Contains(filtro) : true) && ((r.Pista.Instalacion != null && r.Pista.Instalacion.Localidad.ToLower().Contains(localidad.ToLower())) || (r.Pista.Entidad.Localidad.Contains(localidad)))).ToList();
 
-                if (!string.IsNullOrEmpty (orden)) {
-                        switch (orden.ToLower ()) {
+                if (!string.IsNullOrEmpty(orden))
+                {
+                    switch (orden.ToLower())
+                    {
                         case "distancia":
                         default:
-                                if (!string.IsNullOrEmpty (latitud) && !string.IsNullOrEmpty (longitud)) {
-                                        double latitudDouble;
-                                        double longitudDouble;
+                            if (!string.IsNullOrEmpty(latitud) && !string.IsNullOrEmpty(longitud))
+                            {
+                                double latitudDouble;
+                                double longitudDouble;
 
-                                        if (double.TryParse (latitud, out latitudDouble) && double.TryParse (longitud, out longitudDouble)) {
-                                                reservas.Sort ((reserva1, reserva2) =>
-                                                        {
-                                                                double latitud1 = reserva1.Pista.Instalacion != null ? reserva1.Pista.Instalacion.Latitud : reserva1.Pista.Instalacion.Latitud;
-                                                                double longitud1 = reserva1.Pista.Instalacion != null ? reserva1.Pista.Instalacion.Longitud : reserva1.Pista.Instalacion.Longitud;
-                                                                double latitud2 = reserva2.Pista.Instalacion != null ? reserva2.Pista.Instalacion.Latitud : reserva2.Pista.Instalacion.Latitud;
-                                                                double longitud2 = reserva2.Pista.Instalacion != null ? reserva2.Pista.Instalacion.Longitud : reserva2.Pista.Instalacion.Longitud;
-
-                                                                double distancia1 = GeoCalculator.GetDistance (latitudDouble, longitudDouble, latitud1, longitud1, 1, DistanceUnit.Kilometers);
-                                                                double distancia2 = GeoCalculator.GetDistance (latitudDouble, longitudDouble, latitud2, longitud2, 1, DistanceUnit.Kilometers);
-
-                                                                return distancia1.CompareTo (distancia2);
-                                                        });
-                                        }
+                                if (double.TryParse(latitud, out latitudDouble) && double.TryParse(longitud, out longitudDouble))
+                                {
+                                    reservas.OrderBy(reserva =>
+                                    {
+                                        double latitud1 = reserva.Pista.Instalacion != null ? reserva.Pista.Instalacion.Latitud : reserva.Pista.Instalacion.Latitud;
+                                        double longitud1 = reserva.Pista.Instalacion != null ? reserva.Pista.Instalacion.Longitud : reserva.Pista.Instalacion.Longitud;
+                                        double distancia = GeoCalculator.GetDistance(latitudDouble, longitudDouble, latitud1, longitud1, 1, DistanceUnit.Kilometers);
+                                        return distancia;
+                                    }).ToList();
                                 }
-                                break;
+                            }
+                            break;
 
                         case "distancia desc":
-                                if (!string.IsNullOrEmpty (latitud) && !string.IsNullOrEmpty (longitud)) {
-                                        double latitudDouble;
-                                        double longitudDouble;
+                            if (!string.IsNullOrEmpty(latitud) && !string.IsNullOrEmpty(longitud))
+                            {
+                                double latitudDouble;
+                                double longitudDouble;
 
-                                        if (double.TryParse (latitud, out latitudDouble) && double.TryParse (longitud, out longitudDouble)) {
-                                                reservas.Sort ((reserva1, reserva2) =>
-                                                        {
-                                                                double latitud1 = reserva1.Pista.Instalacion != null ? reserva1.Pista.Instalacion.Latitud : reserva1.Pista.Instalacion.Latitud;
-                                                                double longitud1 = reserva1.Pista.Instalacion != null ? reserva1.Pista.Instalacion.Longitud : reserva1.Pista.Instalacion.Longitud;
-                                                                double latitud2 = reserva2.Pista.Instalacion != null ? reserva2.Pista.Instalacion.Latitud : reserva2.Pista.Instalacion.Latitud;
-                                                                double longitud2 = reserva2.Pista.Instalacion != null ? reserva2.Pista.Instalacion.Longitud : reserva2.Pista.Instalacion.Longitud;
-
-                                                                double distancia1 = GeoCalculator.GetDistance (latitudDouble, longitudDouble, latitud1, longitud1, 1, DistanceUnit.Kilometers);
-                                                                double distancia2 = GeoCalculator.GetDistance (latitudDouble, longitudDouble, latitud2, longitud2, 1, DistanceUnit.Kilometers);
-
-                                                                return distancia2.CompareTo (distancia1);
-                                                        });
-                                        }
+                                if (double.TryParse(latitud, out latitudDouble) && double.TryParse(longitud, out longitudDouble))
+                                {
+                                    reservas.OrderByDescending(reserva =>
+                                    {
+                                        double latitud1 = reserva.Pista.Instalacion != null ? reserva.Pista.Instalacion.Latitud : reserva.Pista.Instalacion.Latitud;
+                                        double longitud1 = reserva.Pista.Instalacion != null ? reserva.Pista.Instalacion.Longitud : reserva.Pista.Instalacion.Longitud;
+                                        double distancia = GeoCalculator.GetDistance(latitudDouble, longitudDouble, latitud1, longitud1, 1, DistanceUnit.Kilometers);
+                                        return distancia;
+                                    }).ToList();
                                 }
-                                break;
+                            }
+                            break;
 
                         case "precio":
-                                reservas = reservas.OrderBy (r => r.Pista.Precio).ToList ();
-                                break;
+                            reservas = reservas.OrderBy(r => r.Pista.Precio).ToList();
+                            break;
 
                         case "precio desc":
-                                reservas = reservas.OrderByDescending (r => r.Pista.Precio).ToList ();
-                                break;
+                            reservas = reservas.OrderByDescending(r => r.Pista.Precio).ToList();
+                            break;
 
                         case "valoracion":
-                                reservas = reservas.OrderBy (r => r.Pista.ValoracionesAPistas.Sum (v => v.Estrellas)).ToList ();
-                                break;
+                            reservas = reservas.OrderBy(r => r.Pista.ValoracionesAPistas.Sum(v => v.Estrellas)).ToList();
+                            break;
 
                         case "valoracion desc":
-                                reservas = reservas.OrderByDescending (r => r.Pista.ValoracionesAPistas.Sum (v => v.Estrellas)).ToList ();
-                                break;
-                        }
+                            reservas = reservas.OrderByDescending(r => r.Pista.ValoracionesAPistas.Sum(v => v.Estrellas)).ToList();
+                            break;
+                    }
                 }
 
                 result = reservas;
 
                 if (!notClose)
-                    CPSession.Commit ();
-        }
-        catch (Exception ex)
-        {
+                    CPSession.Commit();
+            }
+            catch (Exception ex)
+            {
                 if (!notClose)
-                    CPSession.RollBack ();
+                    CPSession.RollBack();
                 throw ex;
-        }
-        finally
-        {
+            }
+            finally
+            {
                 if (!notClose)
-                    CPSession.SessionClose ();
+                    CPSession.SessionClose();
+            }
+            return result;
+
+
+            /*PROTECTED REGION END*/
         }
-        return result;
-
-
-        /*PROTECTED REGION END*/
-}
-}
+    }
 }
