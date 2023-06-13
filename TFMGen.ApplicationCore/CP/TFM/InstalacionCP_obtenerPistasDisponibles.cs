7c1c743edb083653ca2678c29260322dd96cb6f9
oskir97 <oscarmarmar97@gmail.com>
2023-06-13 19:03:50 +0200

    correcciones

--- TFMGen.ApplicationCore/CP/TFM/InstalacionCP_obtenerPistasDisponibles.cs
+++ TFMGen.ApplicationCore/CP/TFM/InstalacionCP_obtenerPistasDisponibles.cs
@@ -20,22 +20,22 @@ namespace TFMGen.ApplicationCore.CP.TFM
 public partial class InstalacionCP : GenericBasicCP
 {
 public System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.PistaEN> ObtenerPistasDisponibles (int p_oid, Nullable<DateTime> p_fecha, bool notClose)
-        {
+{
         /*PROTECTED REGION ID(TFMGen.ApplicationCore.CP.TFM_Instalacion_obtenerPistasDisponibles) ENABLED START*/
 
         InstalacionCEN instalacionCEN = null;
-            PistaCP pistaCP = null;
+        PistaCP pistaCP = null;
 
-            System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.PistaEN>  result = null;
+        System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.PistaEN>  result = null;
 
 
         try
         {
                 if (!notClose)
-                    CPSession.SessionInitializeTransaction ();
+                        CPSession.SessionInitializeTransaction ();
 
                 instalacionCEN = new  InstalacionCEN (unitRepo.instalacionrepository);
-                pistaCP = new PistaCP(CPSession, unitRepo);
+                pistaCP = new PistaCP (CPSession, unitRepo);
                 unitRepo.instalacionrepository.setSessionCP (CPSession);
                 unitRepo.pistarepository.setSessionCP (CPSession);
 
@@ -44,27 +44,26 @@ public System.Collections.Generic.IList<TFMGen.ApplicationCore.EN.TFM.PistaEN> O
                 var pistas = instalacionCEN.Obtener (p_oid).Pistas.ToList ();
 
                 foreach (var pista in pistas) {
-                    var horariosDisponibles = pistaCP.Listarhorariosdisponibles(pista.Idpista, p_fecha, true);
+                        var horariosDisponibles = pistaCP.Listarhorariosdisponibles (pista.Idpista, p_fecha, true);
 
-                    if (horariosDisponibles != null && horariosDisponibles.Count() > 0)
-                    {
-                        result.Add(pista);
-                    }
+                        if (horariosDisponibles != null && horariosDisponibles.Count () > 0) {
+                                result.Add (pista);
+                        }
                 }
 
                 if (!notClose)
-                    CPSession.Commit ();
+                        CPSession.Commit ();
         }
         catch (Exception ex)
         {
                 if (!notClose)
-                    CPSession.RollBack ();
+                        CPSession.RollBack ();
                 throw ex;
         }
         finally
         {
                 if (!notClose)
-                    CPSession.SessionClose ();
+                        CPSession.SessionClose ();
         }
         return result;
 
