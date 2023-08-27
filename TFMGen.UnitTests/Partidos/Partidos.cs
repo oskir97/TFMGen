4a0909a02d013cec796a22f4742e1d2a09e84ad3
DESKTOP-5Q3SGUK\Santi <santicastalla15@gmail.com>
2023-08-27 12:57:08 +0200

    Últimas Pruebas

--- TFMGen.UnitTests/Partidos/Partidos.cs
+++ TFMGen.UnitTests/Partidos/Partidos.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using TFMGen.ApplicationCore.CEN.TFM;
 using TFMGen.ApplicationCore.CP.TFM;
 using TFMGen.ApplicationCore.EN.TFM;
@@ -37,16 +38,36 @@ namespace TFMGen.UnitTests.Partidos
             int idpartido = db.reservacen.Crear(usuario.Nombre, usuario.Apellidos, usuario.Email, usuario.Telefono, false, pistaEn.Idpista, 4, horarioEN.Idhorario, DateTime.Now.AddDays(1), TipoReservaEnum.partido, usuario.Idusuario, deporteEN.Iddeporte, -1, NivelPartidoEnum.medio, -1, "Partido de padel para 4 personas", null);
 
             //Inscribirse Partido
-            int idinscripcion = db.reservacen.Crear(usuario.Nombre, usuario.Apellidos, usuario.Email, usuario.Telefono, false, pistaEn.Idpista, 4, horarioEN.Idhorario, DateTime.Now.AddDays(1), TipoReservaEnum.inscripcion, usuario.Idusuario, deporteEN.Iddeporte, -1, null, -1, null, null);
+            int idinscripcion = db.reservacen.Crear(usuario.Nombre, usuario.Apellidos, usuario.Email, usuario.Telefono, 
+                false, pistaEn.Idpista, 4, horarioEN.Idhorario, DateTime.Now.AddDays(1), 
+                TipoReservaEnum.inscripcion, usuario.Idusuario, deporteEN.Iddeporte, -1, null, -1, null, null);
+
             db.reservacp.Inscribirsepartido(idpartido, new List<int> { idinscripcion });
-            
+
+            //Pagos
+            var pago = db.pagotipocen.Listar(0, 1).First();
+            int idpago = db.pagocen.Crear(3.00, 3.63, 0.63, pago.Idtipo, Convert.ToDateTime("12/02/2023 08:00:00"), idinscripcion, null);
+            db.pagocen.Crear(3.00, 3.63, 0.63, pago.Idtipo, DateTime.Now, idinscripcion, null);
+
             ReservaEN partidoCreado = db.reservacen.Obtener(idpartido);
-            var resultado = db.reservacen.Obtenerinscripciones(idinscripcion);
+            //ReservaEN inscripcionCreado = db.reservacen.Obtener(idinscripcion);
+            var resultado = db.reservacen.Obtenerinscripciones(idpartido);
 
             Assert.AreEqual(partidoCreado.Tipo, TipoReservaEnum.partido);
             Assert.AreEqual(partidoCreado.Pista.Idpista, pistaEn.Idpista);
             Assert.AreEqual(partidoCreado.Deporte.Iddeporte, deporteEN.Iddeporte);
             Assert.AreEqual(partidoCreado.Nivelpartido, NivelPartidoEnum.medio);
+        }
+
+        [TestMethod]
+        public void CancelarPartido()
+        {
+            var partido = db.reservacen.Listartodos(0, -1).First();
+            db.reservacen.Cancelar(partido.Idreserva);
+
+            ReservaEN partidoCancelado = db.reservacen.Obtener(partido.Idreserva);
+
+            Assert.AreEqual(partidoCancelado.Cancelada, true);
         }
     }
 }
